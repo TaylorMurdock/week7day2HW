8a6905d846309e2888a46596bc757020ecdd1bd7
Taylor Murdock <tjmurd4545@gmail.com>
2023-06-14 15:58:56 -0400

    setting up database

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+// import dependencies
 const express = require("express");
 const app = express();
 const port = 3000;
+const drinks = require("./models/drinks");
 
+// LISTENER
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Gitpub App!");
+});
+app.get("/drinks", (req, res) => {
+  res.send(drinks);
 });
