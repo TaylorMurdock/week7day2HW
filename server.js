c638f2f2f47081c0c432a7756315238cafe342d7
Taylor Murdock <tjmurd4545@gmail.com>
2023-06-14 16:18:23 -0400

    Setting up  index view

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-// import dependencies
 const express = require("express");
 const app = express();
 const port = 3000;
-const drinks = require("./models/drinks");
 
-// LISTENER
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Configure the view engine
+app.set("view engine", "ejs");
+
+// Require the drinks data from the drinks.js file
+const drinks = require("./models/drinks");
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Gitpub App!");
 });
+
 app.get("/drinks", (req, res) => {
-  res.send(drinks);
+  res.render("index", { drinks: drinks });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
